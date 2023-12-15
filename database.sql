6c5829130222ac47fcac3d488a7eb399a1de2c23
unipen_1_1.pyunipen2022 <unipen_1_1.pyunipen2022@gmail.com>
2023-12-15 09:57:13 +0100

    2.commit

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS tag ;
+DROP TABLE IF EXISTS palya ;
+DROP TABLE IF EXISTS tag   ;
 
 CREATE TABLE tag
 (
@@ -9,8 +10,24 @@ CREATE TABLE tag
    tipus       INTEGER
 ) ;
 
+CREATE TABLE palya
+(
+   id          INTEGER PRIMARY KEY  ,
+   latitude    FLOAT NOT NULL       ,
+   longitude   FLOAT NOT NULL 
+) ;
+
+
 INSERT INTO tag VALUES(1000,'Kiss György'    ,'kiss.gyuri@gmail.com'    ,'06205060170',1) ;
 INSERT INTO tag VALUES(1001,'Horváth Gábor'  ,'horvath.gabi@gmail.com'  ,'06205060171',1) ;
 INSERT INTO tag VALUES(1002,'Németh Anna'    ,'nemeth.anna@gmail.com'   ,'06205060172',1) ;
+
+
+INSERT INTO palya VALUES(1,100,100) ;
+INSERT INTO palya VALUES(2,100,100) ;
+INSERT INTO palya VALUES(3,100,100) ;
+INSERT INTO palya VALUES(4,100,100) ;
+INSERT INTO palya VALUES(5,100,100) ;
+INSERT INTO palya VALUES(6,100,100) ;
 
 SELECT * FROM tag ;
